f6a8c8789c1ca6896df59afdfb2a690ea46eb72e
Sourav R <developer.srv1@gmail.com>
2022-10-09 16:14:13 +0530

    data source as next api

---
+++ pages/api/persons.ts
@@ -0,0 +1,49 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import faker from '@faker-js/faker'
+import type { Person } from '../../src/data/fetchData'
+import { range } from '../../src/utils'
+
+type Response = {
+    rows: Person[]
+    pageCount: number
+}
+
+type Query = {
+    page: string
+    perPage: string
+}
+
+const newPerson = (): Person => {
+    return {
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        age: faker.datatype.number(40),
+        visits: faker.datatype.number(1000),
+        progress: faker.datatype.number(100),
+        status: faker.helpers.shuffle<Person['status']>(['In Relationship', 'Single', 'Complicated'])[0]!,
+    }
+}
+
+export function makeData(...lens: number[]) {
+    const makeDataLevel = (depth = 0): Person[] => {
+        const len = lens[depth]!
+        return range(len).map((d): Person => newPerson())
+    }
+
+    return makeDataLevel()
+}
+
+const data = makeData(10000)
+
+function handler(req: NextApiRequest, res: NextApiResponse<Response>) {
+    const { page, perPage } = <Query>req.query
+    const pageIndex = Number(page)
+    const pageSize = Number(perPage)
+
+    res.status(200).json({
+        rows: data.slice(pageIndex * pageSize, (pageIndex + 1) * pageSize),
+        pageCount: Math.ceil(data.length / pageSize),
+    })
+}
+
+export default handler
--- src/utils.ts
+++ src/utils.ts
@@ -2,4 +2,12 @@ function _shuffle(list: string[]) {
     return list.sort(() => Math.random() - 0.5)
 }
 
-export { _shuffle }
+const range = (len: number) => {
+    const arr = []
+    for (let i = 0; i < len; i++) {
+        arr.push(i)
+    }
+    return arr
+}
+
+export { _shuffle, range }
