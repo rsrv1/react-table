33de1f4e64088100eda087187d15824c75b8c502
Sourav <developer.srv1@gmail.com>
2022-12-10 00:17:13 +0530

    better validating transition

--- src/table/ValidatingIndicator.tsx
+++ src/table/ValidatingIndicator.tsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import clsx from 'clsx'
 import { useLoadingState } from './context/tableContext'
+import styles from './components/loader.module.css'
 
 function ValidatingIndicator() {
     const { validating } = useLoadingState()
+    const [show, setShow] = React.useState(false)
+
+    React.useEffect(() => {
+        if (validating) setShow(true)
+
+        const timer = setTimeout(() => {
+            setShow(false)
+        }, 1200)
+
+        return () => clearTimeout(timer)
+    }, [validating])
 
     return (
         <tr className="relative">
-            <th
-                className={clsx(
-                    'absolute inset-0 transition-[width] duration-700 ease-in-out',
-                    validating ? 'w-full bg-sky-400' : 'w-0 bg-transparent'
-                )}
-            />
+            <th className={clsx('absolute inset-0', show ? styles.validatingTransition : 'bg-transparent')} />
         </tr>
     )
 }
